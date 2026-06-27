e90a379970681ade5feb41ed14c0e431a83908f8
DOWNEYKANE <2900406017@qq.com>
2026-06-27 16:22:20 +0800

    Add files via upload

---
+++ frontend/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        manualChunks: () => 'bundle'
+      }
+    }
+  }
+})
